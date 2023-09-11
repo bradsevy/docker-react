92ad777eeb8cf9613d2f483970ce0140ad940621
Brad <bsevy@gitlab.com>
2023-09-11 12:44:27 -0400

    edit App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Espagetti!
+          Espagetti feature branch.
         </p>
         <a
           className="App-link"
